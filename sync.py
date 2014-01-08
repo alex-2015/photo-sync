21eefc8a8fe6cbae22b5c677e8f8ba4036fe2295
Neil McGuiggan <neilmcg@yahoo-inc.com>
2014-01-08 14:47:16 +0000

    Changed to use photo exif data instead of file creation time

--- sync.py
+++ sync.py
@@ -7,6 +7,33 @@ import time
 from datetime import datetime
 import argparse
 import re
+from PIL import Image
+from PIL.ExifTags import TAGS
+
+def get_exif_data(filename):
+  ret = {}
+  try:
+    image = Image.open(filename)
+    info = image._getexif()
+    for tag, value in info.items():
+      decoded = TAGS.get(tag, tag)
+      ret[decoded] = value
+    return ret
+  except IOError:
+    print 'Error loading file EXIF data'
+    return None
+    
+def get_photo_date(exif_data):
+  date_string = exif_data['DateTimeOriginal']
+  return datetime.strptime(date_string, "%Y:%m:%d %H:%M:%S")
+  
+def get_photo_dir(photo_date):
+  month = str(photo_date.month)
+  if (len(month) == 1):
+    month = "0" + month;
+  return str(photo_date.year) + "-" + month
+  
+
 
 argParser = argparse.ArgumentParser(description='Script to process photos from source and move them to dest.')
 argParser.add_argument('source', help='source directory - absolute path')
@@ -45,18 +72,26 @@ error_files = []
 for root, subFolders, files in os.walk(sourceDir):
   for photo_file in files:
     try:
-      if photo_file.endswith((".jpg", ".jpeg", ".png", ".gif")):
+      if photo_file.endswith((".png", ".jpg", ".jpeg", ".gif")):
         photoPath = os.path.join(root, photo_file)
-        photo_date = datetime.fromtimestamp(os.path.getmtime(photoPath))
+        
+        photo_data = get_exif_data(photoPath)
+        
+        if (photo_data is None):
+          print 'No exif data found'
+          error_files.append(photoPath)
+        else:
+          photo_date = get_photo_date(photo_data)
+          photo_dir = get_photo_dir(photo_date)
+          new_photo_dir = os.path.join(destDir, photo_dir)
+          #if no directory exists for photo_date, create it
+          if not os.path.exists(new_photo_dir) and not os.path.isdir(new_photo_dir):
+            os.mkdir(new_photo_dir)
 
-        #if no directory exists for photo_date, create it
-        new_photo_dir = os.path.join(destDir, photo_date.strftime("%Y-%m"))
-        if not os.path.exists(new_photo_dir) and not os.path.isdir(new_photo_dir):
-          os.mkdir(new_photo_dir)
-
-        shutil.copy2(photoPath, new_photo_dir)
-        os.remove(photoPath)
+          shutil.copy2(photoPath, new_photo_dir)
+          os.remove(photoPath)
     except Exception:
+      print 'exception somewhere'
       error_files.append(photoPath)
 
 for error_file in error_files:
